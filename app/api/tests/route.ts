c55b6fa565ac081aa5d78a99896df462ce2e1b37
Jack Youkstetter <jackyoukstetter1@gmail.com>
2024-05-02 01:36:51 -0700

    code merge fix

--- app/api/tests/route.ts
+++ app/api/tests/route.ts
@@ -18,7 +18,6 @@ export async function POST(request: NextRequest) {
     },
   });
   if (!user) return NextResponse.json({ status: 400, message: "No user found" });
-  if (user.subscriptionStatus != "active" && user.generations >= 3) return NextResponse.json({ status: 400, message: "Test generation limit reached" });
 
   const {
     owner,
